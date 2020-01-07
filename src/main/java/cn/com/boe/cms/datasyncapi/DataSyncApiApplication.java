e862f1696bf5765efac2696a62d0b5a29d061836
wumingsheng <wu_mingsheng@126.com>
2020-01-07 17:53:47 +0800

    init

---
+++ src/main/java/cn/com/boe/cms/datasyncapi/DataSyncApiApplication.java
@@ -0,0 +1,13 @@
+package cn.com.boe.cms.datasyncapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSyncApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataSyncApiApplication.class, args);
+	}
+
+}
